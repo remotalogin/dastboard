5731a965fc7a75439b51557ec89d0ae538133d02
remotalogin <remotamobile@gmail.com>
2025-09-18 21:26:09 -0300

    Update historico.js

--- api/historico.js
+++ api/historico.js
@@ -3,16 +3,19 @@ import fetch from 'node-fetch';
 export default async (req, res) => {
   try {
     const response = await fetch('https://api.jogosvirtual.com/jsons/historico_baralho_footballstudio.json?' + Date.now());
+    if (!response.ok) {
+        throw new Error(`Erro na API externa: ${response.status}`);
+    }
     const data = await response.json();
     
     // Configura os cabeçalhos para permitir CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     
-    // Envia os dados da API original como resposta
     res.status(200).send(data);
 
   } catch (error) {
+    console.error('Erro no proxy:', error);
     res.status(500).send({ error: 'Erro ao buscar dados da API externa.' });
   }
 };
